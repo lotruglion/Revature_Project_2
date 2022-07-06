e93057b8cabc97875743e0f4235ead841d2616d7
Nicolas <nlotruglio@gmail.com>
2022-07-06 19:26:18 -0400

    generating random reasons as to why transactions failed

--- Project_2.py
+++ Project_2.py
@@ -1,16 +1,56 @@
 #from pyspark.sql import SparkSession
 
+from faker import Faker
+
 import csv
+import random
 
-header = ['order_id', 'customer_id', 'customer_name', 'product_name', 'product_category', 'payment_type', 'qty', 'price', 'datetime', 'country', 'city', 'ecommerce_website_name', 'payment_txn_id', 'payment_txn_success', 'failure_reason']
+# header = ['order_id', 'customer_id', 'customer_name', 'product_name', 'product_category', 'payment_type', 'qty', 'price', 'datetime', 'country', 'city', 'ecommerce_website_name', 'payment_txn_id', 'payment_txn_success', 'failure_reason']
 
-data = ['1', '1','1','1','1','1','1','1','1','1','1','1','1','1','1','1',]
+# data = ['1', '1','1','1','1','1','1','1','1','1','1','1','1','1','1','1',]
 
-f = open('project_2_data', 'w')
+# f = open('project_2_data', 'w')
 
-writer = csv.writer(f)
+# writer = csv.writer(f)
 
-writer.writerow(header)
-writer.writerow(data)
+# writer.writerow(header)
+# writer.writerow(data)
 
-f.close()
+# f.close()
+
+
+# doclist=["Physician", "Specialist"]
+# fake=Faker()
+# mylist = ["Male", "Female", "Other"]
+# i=1
+# while i<51:
+#names.get_full_name(), random.randrange(82)+18, mylist[random.randrange(3)]
+
+#JiaYing's code
+# try:
+#     createfile=open('name.csv','x')
+#     createfile.close
+# except FileExistsError:
+#     print('File already exist overwriting')
+# filename="name.csv"
+# fakedata = Faker()
+# fakedata.name()
+# namelist=[]
+# for i in range(3):
+#     namelist.append(fakedata.name())
+#     print(namelist[i])
+# with open(filename,'w',newline='') as csvfile:
+#     namewriter=csv.writer(csvfile, delimiter=',')
+#     namewriter.writerows([namelist])
+
+
+Y = True
+N = False
+
+declinedTransaction = ["Card was declined", "Order Cancelled", "Out of Stock", "Card was declined", "Order Cancelled", "Out of Stock"]
+
+while True:
+    print(random.choice(declinedTransaction))
+    break
+else:
+    print("")
