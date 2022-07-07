f62d8c894c4be2bcb50938104d5b2ffa9df1f96d
rileywakeman <106113694+rileywakeman@users.noreply.github.com>
2022-07-07 12:56:34 -0600

    Add files via upload

--- ##project 2.py
+++ ##project 2.py
@@ -1,52 +1,47 @@
 ##project 2
-
-from itertools import product
 import os 
 import pandas as pd
 import numby
 import random
 import datetime
 
-products = {
-    'Bread': 2.79,
-    'Eggs': 4.00,
-    'Rice': 1.70,
-    'Butter': 2.92,
-    'Milk': 2.99
+commerce = {
+    'www.amazon.com' : 1,
+    'www.eatfresh.com' : 5
+    
 } 
-def generate_random_address():
-    street_name = ['Main']
-    cities = ['Colorado']
-    country = ['America FY']
+def generate_random_ecommerceemail():
+    ecommercemail = ['www.amazon.com', 'www.eatfresh.com']
+ 
+##We have order_id, customer_id, customer_name, product_id, product_name, product_category, qty, price, country, city, 
+## Do the datetime, and ecommerce_website_name  
+##columns = ['order_id', 'customer_id', 'customer_name', 'product_id', 'product_name', 'product_category', 'payment_type', 'qty', 'price', 'datetime', 'country', 'city', 'ecommerce_website_name', 'payment_txn_id', 'payment_success', 'faliure_reason']
+columns = ['ecommerce_website_name']
 
-product_list =     
-columns = ['order_id', 'customer_id', 'customer_name', 'product_id', 'product_name', 'product_category', 'payment_type', 'qty', 'price', 'datetime', 'country', 'city', 'ecommerce_website_name', 'payment_txn_id', 'payment_success', 'faliure_reason']
-order_id = 1
-customer_id = 1
-##product_id = 1
 df = pd.DataFrame(columns = columns)
+##.keys()
+for i in range(10):
+    listcommerce = [ecommerce for ecommerce in commerce]
+    weights = [commerce[ecommerce][0]for commerce in ecommerce]
+    ecommerce = random.choices(listcommerce, weights = weights)[0]
+    price = commerce[ecommerce]
+    df.loc[i] = [i, ecommerce, 1]
 
-for i in range(order_amount):
-    product = random.choices(products.keys())[0]
-    price = products[product]
-    df.loc[i] = [i, product, 1, price, ]
-
-    order_id += 1
-    customer_id += 1
-    ##prodcut_id = 
+print(ecommerce)
+print(weights)
 """
 df.to_csv('test_data.cvs')
 
 path = "*have a file name*"
 files = [file for file in os.listdir(path) if not file.startswitch('.')]
 
-all_month_data = pd.DataFrame()
+data1 = pd.DataFrame()
 
 for file in files:
     current_data = pd.read_csv(path+"/"+file)
-    all_months_data = pd.concat([all_months_data, current_data])
+    data1 = pd.concat([data1, current_data])
 
-all_months_data.to_csv("all_data_copy.csv", index = False)
+data1.to_csv("all_data_copy.csv", index = False)
 
 all_data = pd.read_csv("all_data.csv")
 all.data.head(25)
