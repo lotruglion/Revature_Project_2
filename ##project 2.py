37e6074f7fa11779e9c0390f3e48005255583980
rileywakeman <106113694+rileywakeman@users.noreply.github.com>
2022-07-07 15:24:19 -0600

    Add files via upload

--- ##project 2.py
+++ ##project 2.py
@@ -3,33 +3,40 @@ import os
 import pandas as pd
 import random
 import datetime
+import csv
 
 commerce = {
-    'www.amazon.com' : 1,
-    'www.eatfresh.com' : 5
-    
+    ## site and weights
+    'www.amazon.com' : [24],
+    'www.eatfresh.com' : [16],
+    'www.eathumanflesh.com' : [1],
+    'www.gogofoods.com' : [12],
+    'www.gainzfood.com' : [3],
+    'www.notsafefoodforyou' : [4]
+
 } 
- 
+ ## the site names are for practice sake not for final product
+
 ##We have order_id, customer_id, customer_name, product_id, product_name, product_category, qty, price, country, city, 
 ## Do the datetime, and ecommerce_website_name  
 ##columns = ['order_id', 'customer_id', 'customer_name', 'product_id', 'product_name', 'product_category', 'payment_type', 'qty', 'price', 'datetime', 'country', 'city', 'ecommerce_website_name', 'payment_txn_id', 'payment_success', 'faliure_reason']
 columns = ['ecommerce_website_name']
 
 df = pd.DataFrame(columns = columns)
-##.keys()
+##save for just in case .keys()
 for i in range(10):
+    ##list comprehension
     listcommerce = [ecommerce for ecommerce in commerce]
-    weights = [commerce[ecommerce][0]for commerce in ecommerce]
+    ##defining weights
+    weights = [commerce[ecommerce][0]for ecommerce in commerce]
     ecommerce = random.choices(listcommerce, weights = weights)[0]
+    ##ecommerce = random.choice(list(commerce, weights = weights))[0]
     price = commerce[ecommerce]
     df.loc[i] = [i, ecommerce, 1]
 
-print(ecommerce)
-print(weights)
-"""
 df.to_csv('test_data.cvs')
 
-path = "*have a file name*"
+path = "eccommerce mail"
 files = [file for file in os.listdir(path) if not file.startswitch('.')]
 
 data1 = pd.DataFrame()
@@ -42,4 +49,3 @@ data1.to_csv("all_data_copy.csv", index = False)
 
 all_data = pd.read_csv("all_data.csv")
 all.data.head(25)
-"""
