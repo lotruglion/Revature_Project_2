4a311395e8350d32af073131bfebf9c6164ca244
jduenas1 <duenasjiaying@gmail.com>
2022-07-13 10:06:19 -0500

    cleaned

--- test.py
+++
@@ -1,7 +0,0 @@
-from faker import Faker 
-from faker.providers import date_time
-
-test1 = Faker()
-
-for i in range(2):
-  print(test1.date(pattern: str = '%2021-%01-%01', end_datetime: Union[datetime.date, datetime.datetime, datetime.timedelta, str, int, None] = None)))
